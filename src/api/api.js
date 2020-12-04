d1567eeb8362a759fa0947572ce69cc64e32ea73
ulyan0v <73800859+ulyan0v@users.noreply.github.com>
2020-12-04 22:59:23 +0300

    added base api

---
+++ src/api/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_KEY = 'a072a71ffdf745449e81cc972606db86';
+
+const api = axios.create({
+  baseURL: 'https://newsapi.org/v2/'
+});
+
+export const getNews = page => {
+  return api.get(`top-headlines?country=ru&apiKey=${API_KEY}&page=${page}`);
+}
